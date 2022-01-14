09a25634cb8fded2d9e6adb997c30e017dd366c4
pujitha <siripujitha9701@gamil.com>
2022-01-14 19:41:55 +0530

    full-stack

--- script.js
+++ script.js
@@ -10,16 +10,25 @@ loadImage("/images/idle/1.png",(img)=>{
     c.drawImage(img,0,0,500,500);
 })
 
-let imagePath=(frame)=>{
-    return "/images/idle/"+frame+".png";
+let imagePath=(frame,animation)=>{
+    return "/images/"+animation+"/"+frame+".png";
 }
+let frames={
+    idle:[1,2,3,4,5,6,7,8],
+    kick:[1,2,3,4,5,6,7],
+    punch:[1,2,3,4,5,6,7],
+
+};
 let loadImages=(callback)=>{
-    let imageload=8;
-    let image=[];
-    [1,2,3,4,5,6,7,8].forEach((frame)=>{
-        let path=imagePath(frame);
+    let imageload=0;
+    let image={idle:[],kick:[],punch:[]};
+    ["idle","kick","punch"].forEach((animation)=>{
+        let animationFrame=frames[animation];
+        imageload=imageload+animationFrame.length;
+        animationFrame.forEach((frame)=>{
+        let path=imagePath(frame,animation);
         loadImage(path,(imag)=>{
-          image[frame-1]=imag;
+          image[animation][frame-1]=imag;
           imageload=imageload-1;
           if(imageload===0)
           {
@@ -27,9 +36,10 @@ let loadImages=(callback)=>{
           }
         });
     });
+    });
 };
-let animate=(c,image,callback)=>{
-    image.forEach((image,index)=>{
+let animate=(c,image,animation,callback)=>{
+    image[animation].forEach((image,index)=>{
         setTimeout(()=>{
             c.clearRect(0,0,500,500);
            c.drawImage(image,0,0,500,500);
@@ -38,5 +48,33 @@ let animate=(c,image,callback)=>{
     setTimeout(callback,image.length*100);
 };
 loadImages((image)=>{
- animate(c,image,()=>{console.log("done");});
+    let queued=[];
+    let aux=()=>
+    {
+        let selected;
+        if(queued.length===0)
+         selected="idle";
+        else
+          selected=queued.shift(); 
+        animate(c,image,selected,aux);
+};
+aux();
+document.getElementById("kick").onclick=()=>{
+queued.push("kick");
+};
+document.getElementById("punch").onclick=()=>{
+    queued.push("punch");
+};
+document.addEventListener("keyup",(event)=>{
+    const key=event.key;
+    if(key==="ArrowLeft")
+    {
+        queued.push("kick");
+    }
+    else if(key==="ArrowRight")
+    {
+        queued.push("punch");
+    }
 });
+});
+ 
